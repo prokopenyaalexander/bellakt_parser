2a4454fd01a657940e71c58a1934fb5811789b9d
Aliaksandr <prokopenya_a@mail.ru>
2024-10-14 16:36:12 +0300

    finished add_urls_to_pricing_module script

--- config/config_queries.py
+++ config/config_queries.py
@@ -1,6 +1,6 @@
-from sqlalchemy import text, insert, Table, MetaData, select, update, func, delete
+from sqlalchemy import text, insert, Table, MetaData, select, update, func, delete, and_
 from sqlalchemy.orm import sessionmaker, Session
-from config.models import SiteSet, RankingProducts
+from config.models import SiteSet, RankingProducts, UrlsToCrawling
 from config.orm_core import engine
 import logging
 import datetime
@@ -28,6 +28,7 @@ metadata = MetaData()
 site_tree = Table('site_tree', metadata, autoload_with=engine)
 siteset_orm = Table('siteset_orm', metadata, autoload_with=engine)
 ranking_products_orm = Table('ranking_products_orm', metadata, autoload_with=engine)
+urls_to_crawling_orm = Table('urls_to_crawling_orm', metadata, autoload_with=engine)
 
 #  common block
 
@@ -93,6 +94,7 @@ def insert_to_ranking_products(name, cnt_products, url, dt):
         connection.rollback()
         logger.error(f"Error while inserting data: {str(e)}")
 
+# функция обновления записей
 def udp_record_in_ranking_products(name, cnt_products, url, dt):
     try:
         with (engine.connect() as connection):
@@ -116,6 +118,7 @@ def udp_record_in_ranking_products(name, cnt_products, url, dt):
         connection.rollback()
         logger.error(f"Error while inserting data: {str(e)}")
 
+# функция удаления дубликатов
 def count_records_in_ranking_products(category_name, date_of_insertion):
     try:
         with engine.connect() as connection:
@@ -130,6 +133,7 @@ def count_records_in_ranking_products(category_name, date_of_insertion):
         print(f"Error while executing SELECT COUNT query: {str(e)}")
         return  result
 
+# функция поиска дубликатов
 SessionLocal = sessionmaker(bind=engine)
 def find_duplicates():
     session = SessionLocal()
@@ -158,8 +162,7 @@ def find_duplicates():
     finally:
         session.close()  # Закрываем сессию
 
-
-# Вызов функции для поиска дубликатов
+# функция удаления дубликатов
 def remove_duplicates():
     session = SessionLocal()
     try:
@@ -191,5 +194,136 @@ def remove_duplicates():
     finally:
         session.close()  # Закрываем сессию
 
+
+# Pricing block
+def select_all_from_site_set_two():
+    today = date.today()
+    try:
+        with engine.connect() as connection:
+            stmt = select(SiteSet.name, SiteSet.url).where(
+                and_(func.date(SiteSet.created_at) == today)
+            )
+            result = connection.execute(stmt)
+            records = result.fetchall()
+    except Exception as e:
+        print(f"Error while executing SELECT query: {str(e)}")
+    return records
+
+# print(select_all_from_site_set_two())
+
+def insert_to_urls_to_crawling_orm(name, url, dt):
+    try:
+        with engine.connect() as connection:
+            insert_stmt = insert(urls_to_crawling_orm).values(
+                pricing_url=name,
+                category_url=url,
+                date=dt
+            )
+            connection.execute(insert_stmt)
+            connection.commit()
+            logger.info(f'Data inserted: {name},  {url}')
+    except Exception as e:
+        connection.rollback()
+        logger.error(f"Error while inserting data: {str(e)}")
+
+# функция обновления записей
+def udp_record_in_urls_to_crawling_orm(name, cnt_products, url, dt):
+    try:
+        with (engine.connect() as connection):
+            update_stmt = (
+            update(RankingProducts)
+            .where(
+                (RankingProducts.category_name == name) &
+                (RankingProducts.date == dt)
+            )
+            .values(
+                category_name=name,
+                count_of_products=cnt_products,
+                category_url=url,
+                date=dt
+                )
+            )
+            connection.execute(update_stmt)
+            connection.commit()
+            logger.info(f'Data updated: {name}, {cnt_products}, {url}')
+    except Exception as e:
+        connection.rollback()
+        logger.error(f"Error while inserting data: {str(e)}")
+
+# функция удаления дубликатов
+def count_records_in_urls_to_crawling_orm(category_name, date_of_insertion):
+    try:
+        with engine.connect() as connection:
+            # Создаем запрос для подсчета записей
+            stmt = select(func.count()).filter(
+                (RankingProducts.category_name == category_name) &
+                (RankingProducts.date == date_of_insertion)
+            )
+            result = connection.execute(stmt)
+
+    except Exception as e:
+        print(f"Error while executing SELECT COUNT query: {str(e)}")
+        return  result
+
+# функция поиска дубликатов
+# SessionLocal = sessionmaker(bind=engine)
+def find_duplicates_urls_to_crawling_orm():
+    session = SessionLocal()
+    try:
+        # Создаем запрос для поиска дубликатов
+        duplicates_stmt = select(
+            UrlsToCrawling.pricing_url,
+            UrlsToCrawling.date,
+            func.count().label('count')
+        ).group_by(
+            UrlsToCrawling.pricing_url,
+            UrlsToCrawling.date
+        ).having(func.count() > 1)
+
+        # Выполняем запрос
+        duplicates = session.execute(duplicates_stmt).fetchall()
+
+        if duplicates:
+            for dup in duplicates:
+                print(f"Category URL: {dup.pricing_url}, Date: {dup.date}, Count: {dup.count}")
+        else:
+            print("No duplicates found.")
+
+    except Exception as e:
+        print(f"Error while finding duplicates: {str(e)}")
+    finally:
+        session.close()  # Закрываем сессию
+
+# функция удаления дубликатов
+def remove_duplicates_urls_to_crawling_orm():
+    session = SessionLocal()
+    try:
+        # Подзапрос для получения id дубликатов
+        subquery = (
+            select(
+                UrlsToCrawling.id,
+                func.row_number().over(partition_by=[UrlsToCrawling.pricing_url, UrlsToCrawling.date]).label('rownum')
+            )
+            .subquery()
+        )
+
+        # Запрос на удаление дубликатов
+        delete_stmt = delete(UrlsToCrawling).where(
+            UrlsToCrawling.id.in_(
+                select(subquery.c.id).where(subquery.c.rownum > 1)
+            )
+        )
+
+        # Выполнение запроса на удаление
+        result = session.execute(delete_stmt)
+        session.commit()  # Сохраняем изменения
+
+        logging.info(f"Removed {result.rowcount} duplicate records.")
+
+    except Exception as e:
+        session.rollback()  # Откат транзакции в случае ошибки
+        logging.error(f"Error while removing duplicates: {str(e)}")
+    finally:
+        session.close()  # Закрываем сессию
 
 
