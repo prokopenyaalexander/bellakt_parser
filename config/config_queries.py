e06353451c5173ed6cc7b8cf89f7aa7bdaaf0a36
Aliaksandr <prokopenya_a@mail.ru>
2024-10-14 14:55:20 +0300

    finished get_count_ranking_products script

--- config/config_queries.py
+++ config/config_queries.py
@@ -1,12 +1,14 @@
-from sqlalchemy import text, insert, Table, MetaData, select, update
+from sqlalchemy import text, insert, Table, MetaData, select, update, func, delete
 from sqlalchemy.orm import sessionmaker, Session
-
-from config.orm_config import engine
+from config.models import SiteSet, RankingProducts
+from config.orm_core import engine
 import logging
 import datetime
 import os
 from config.time_config import time_format
 from config.paths_config import queries_log_directory
+# from orm_core import create_connection
+
 
 
 date = datetime.date.today()
@@ -24,7 +26,8 @@ logger.addHandler(handler)
 
 metadata = MetaData()
 site_tree = Table('site_tree', metadata, autoload_with=engine)
-
+siteset_orm = Table('siteset_orm', metadata, autoload_with=engine)
+ranking_products_orm = Table('ranking_products_orm', metadata, autoload_with=engine)
 
 #  common block
 
@@ -40,12 +43,11 @@ def clear_table():
 
 #  site_set block
 
-
-def write_to_db(name, url, created_at):
+def write_to_db_site_set(name, url, created_at):
     try:
         with engine.connect() as connection:
             # Создаем запрос на вставку
-            insert_stmt = insert(site_tree).values(name=name, url=url, created_at=created_at)
+            insert_stmt = insert(siteset_orm).values(name=name, url=url, created_at=created_at)
             connection.execute(insert_stmt)
             connection.commit()
             logger.info(f'Data inserted: {name}, {url}, {created_at}')
@@ -55,23 +57,139 @@ def write_to_db(name, url, created_at):
 
 #  get_count_ranking_products block
 
-
-def select_all_from_site_tree():
-    Session = sessionmaker(engine)
-    session = Session()
+def select_all_from_site_set():
     try:
-        select_stmt = select(site_tree.c.name, site_tree.c.url)
-        result = session.execute(select_stmt)
-        records = result.fetchall()
-        for record in records:
-            print(record)
+        with engine.connect() as connection:
+            stmt = select(SiteSet.name, SiteSet.url)
+            result = connection.execute(stmt)
+            records = result.fetchall()
     except Exception as e:
         print(f"Error while executing SELECT query: {str(e)}")
+    return records
 
+def select_all_from_ranking_products():
+    try:
+        with engine.connect() as connection:
+            stmt = select(RankingProducts.category_name, RankingProducts.date)
+            result = connection.execute(stmt)
+            records = result.fetchall()
+    except Exception as e:
+        print(f"Error while executing SELECT query: {str(e)}")
+    return records
+
+def insert_to_ranking_products(name, cnt_products, url, dt):
+    try:
+        with engine.connect() as connection:
+            insert_stmt = insert(ranking_products_orm).values(
+                category_name=name,
+                count_of_products=cnt_products,
+                category_url=url,
+                date=dt
+            )
+            connection.execute(insert_stmt)
+            connection.commit()
+            logger.info(f'Data inserted: {name}, {cnt_products}, {url}')
+    except Exception as e:
+        connection.rollback()
+        logger.error(f"Error while inserting data: {str(e)}")
+
+def udp_record_in_ranking_products(name, cnt_products, url, dt):
+    try:
+        with (engine.connect() as connection):
+            update_stmt = (
+            update(RankingProducts)
+            .where(
+                (RankingProducts.category_name == name) &
+                (RankingProducts.date == dt)
+            )
+            .values(
+                category_name=name,
+                count_of_products=cnt_products,
+                category_url=url,
+                date=dt
+                )
+            )
+            connection.execute(update_stmt)
+            connection.commit()
+            logger.info(f'Data updated: {name}, {cnt_products}, {url}')
+    except Exception as e:
+        connection.rollback()
+        logger.error(f"Error while inserting data: {str(e)}")
+
+def count_records_in_ranking_products(category_name, date_of_insertion):
+    try:
+        with engine.connect() as connection:
+            # Создаем запрос для подсчета записей
+            stmt = select(func.count()).filter(
+                (RankingProducts.category_name == category_name) &
+                (RankingProducts.date == date_of_insertion)
+            )
+            result = connection.execute(stmt)
+
+    except Exception as e:
+        print(f"Error while executing SELECT COUNT query: {str(e)}")
+        return  result
+
+SessionLocal = sessionmaker(bind=engine)
+def find_duplicates():
+    session = SessionLocal()
+    try:
+        # Создаем запрос для поиска дубликатов
+        duplicates_stmt = select(
+            RankingProducts.category_url,
+            RankingProducts.date,
+            func.count().label('count')
+        ).group_by(
+            RankingProducts.category_url,
+            RankingProducts.date
+        ).having(func.count() > 1)
+
+        # Выполняем запрос
+        duplicates = session.execute(duplicates_stmt).fetchall()
+
+        if duplicates:
+            for dup in duplicates:
+                print(f"Category URL: {dup.category_url}, Date: {dup.date}, Count: {dup.count}")
+        else:
+            print("No duplicates found.")
+
+    except Exception as e:
+        print(f"Error while finding duplicates: {str(e)}")
     finally:
-        session.close()
-        # logger.error(f"Error while inserting data: {str(e)}")
+        session.close()  # Закрываем сессию
 
 
-select_all_from_site_tree()
+# Вызов функции для поиска дубликатов
+def remove_duplicates():
+    session = SessionLocal()
+    try:
+        # Подзапрос для получения id дубликатов
+        subquery = (
+            select(
+                RankingProducts.id,
+                func.row_number().over(partition_by=[RankingProducts.category_url, RankingProducts.date]).label('rownum')
+            )
+            .subquery()
+        )
+
+        # Запрос на удаление дубликатов
+        delete_stmt = delete(RankingProducts).where(
+            RankingProducts.id.in_(
+                select(subquery.c.id).where(subquery.c.rownum > 1)
+            )
+        )
+
+        # Выполнение запроса на удаление
+        result = session.execute(delete_stmt)
+        session.commit()  # Сохраняем изменения
+
+        logging.info(f"Removed {result.rowcount} duplicate records.")
+
+    except Exception as e:
+        session.rollback()  # Откат транзакции в случае ошибки
+        logging.error(f"Error while removing duplicates: {str(e)}")
+    finally:
+        session.close()  # Закрываем сессию
+
+
 
--- customers/bellakt/ranking/get_count_ranking_products.py
+++ customers/bellakt/ranking/get_count_ranking_products.py
@@ -4,8 +4,10 @@ import os
 import psycopg2
 import requests
 from bs4 import BeautifulSoup
-from psycopg2 import OperationalError
+from config.config_queries import (select_all_from_site_set, insert_to_ranking_products, find_duplicates,
+                                   remove_duplicates)
 from config.db_config import create_connection
+from config.models import RankingProducts
 from config.paths_config import ranking
 from config.time_config import time_format
 
@@ -23,116 +25,79 @@ logging.basicConfig(
 )
 
 
-def get_products_count(connection):
-    query = "SELECT name, url from site_tree where shop_id=101"
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        logging.info(f"Data Extracted from site_tree table. QUERY: {query} ")
-        result = cursor.fetchall()
-    except OperationalError as e:
-        logging.error(f"Error has happend: {e}")
-    logging.info(f"Starting processing data")
+class RankProds:
+    # def __init__(self, connection):
+    #     self.connection = connection
 
-    for row in result:
-        url = row[1]
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        if soup.find("div", class_="nums"):
-            number_of_pages = int(soup.find("div", class_="nums").find_all("a", class_="dark_link")[-1].text)
-            process_multiple_pages_data(url, number_of_pages, connection)
-        else:
-            process_single_page_data(url, connection)
-
-
-def process_multiple_pages_data(url, number_of_pages, connection):
-    base_category_url = url
-    response = requests.get(base_category_url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    category_name = soup.find("div", class_='topic__heading').find("h1").get_text()
-    count_of_products = 0
-    page_param = f'?PAGEN_1='
-    urls_to_crawl = [f'{url}{page_param}{gen_url}' for gen_url in range(2, number_of_pages+1)]
-    urls_to_crawl.insert(0, url)
-    category_url = base_category_url
-    for url in urls_to_crawl:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        products_links = soup.find_all("div", class_='inner_wrap TYPE_1')
-        count_of_products += len(products_links)
-    try:
-        with connection.cursor() as cur:
-            date_of_insertion = datetime.date.today()
-            cur.execute(
-                "SELECT COUNT(*) FROM ranking_products WHERE category_name = %s AND date::date = %s",
-                (category_name, date_of_insertion)
-            )
-            exists = cur.fetchone()[0] > 0
-            if exists:
-                # Обновление существующей записи
-                cur.execute(
-                    "UPDATE ranking_products SET category_name = %s, date = %s, category_url = %s "
-                    "WHERE category_name = %s AND date::date = %s",
-                    (category_name, date_of_insertion, category_url, category_name, date_of_insertion)
-                )
-                logging.info(f"Updated data for category_name: {category_name}, "
-                             f"url - {category_url} {date_of_insertion}")
+    def get_products_count(self):
+        records = select_all_from_site_set()
+        for row in records:
+            url = row[1]
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            if soup.find("div", class_="nums"):
+                number_of_pages = int(soup.find("div", class_="nums").find_all("a", class_="dark_link")[-1].text)
+                self.process_multiple_pages_data(url, number_of_pages)
             else:
-                # Вставка новой записи
-                cur.execute(
-                    "INSERT INTO ranking_products (category_name, count_of_products, date, category_url)"
-                    " VALUES (%s, %s, %s, %s)",
-                    (category_name, count_of_products, date_of_insertion, category_url)
-                )
-                logging.info(f"Inserted data for category_name {category_name}, url - {category_url} "
-                             f"count_of_products: {count_of_products} in process_multiple_pages_data")
-            connection.commit()  # Выполняем коммит после успешной вставки
-    except psycopg2.Error as e:
-        logging.error(f"Error inserting data in process_multiple_pages_data: {e}")
-        connection.rollback()
-    logging.info("Data insertion completed in process_multiple_pages_data.")
+                self.process_single_page_data(url)
 
 
-def process_single_page_data(url, connection):
-    base_category_url = url
-    category_url = base_category_url
-    response = requests.get(base_category_url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    category_name = soup.find("div", class_='topic__heading').find("h1").get_text()
-    count_of_products = len(soup.find_all("div", class_='inner_wrap TYPE_1'))
-    try:
-        with connection.cursor() as cur:
+    def process_multiple_pages_data(self, url, number_of_pages):
+        base_category_url = url
+        response = requests.get(base_category_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        category_name = soup.find("div", class_='topic__heading').find("h1").get_text()
+        count_of_products = 0
+        page_param = f'?PAGEN_1='
+        urls_to_crawl = [f'{url}{page_param}{gen_url}' for gen_url in range(2, number_of_pages+1)]
+        urls_to_crawl.insert(0, url)
+        category_url = base_category_url
+        for url in urls_to_crawl:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            products_links = soup.find_all("div", class_='inner_wrap TYPE_1')
+            count_of_products += len(products_links)
+
+        try:
             date_of_insertion = datetime.date.today()
-            cur.execute(
-                "SELECT COUNT(*) FROM ranking_products WHERE category_name = %s AND date::date = %s ",
-                (category_name, date_of_insertion)
-            )
-            exists = cur.fetchone()[0] > 0
-            if exists:
-                # Обновление существующей записи
-                cur.execute(
-                    "UPDATE ranking_products SET category_name = %s, date = %s, category_url = %s "
-                    "WHERE category_name = %s AND date::date = %s",
-                    (category_name, date_of_insertion, category_url, category_name, date_of_insertion)
-                )
-                logging.info(f"Updated data for category_name: {category_name}, "
-                             f"url - {category_url} {date_of_insertion}")
-            else:
-                # Вставка новой записи
-                cur.execute(
-                    "INSERT INTO ranking_products (category_name, count_of_products, date, category_url) "
-                    "VALUES (%s, %s, %s, %s)",
-                    (category_name, count_of_products, date_of_insertion, category_url)
-                )
+
+            insert_to_ranking_products(category_name, count_of_products, category_url, date_of_insertion)
+
             logging.info(f"Inserted data for category_name {category_name}, url - {category_url} "
-                         f"count_of_products: {count_of_products} in process_single_page_data")
-            connection.commit()  # Выполняем коммит после успешной вставки
-    except psycopg2.Error as e:
-        logging.error(f"Error inserting data in process_single_page_data: {e}")
-        connection.rollback()
-    logging.info("Data insertion completed in process_single_page_data.")
+                             f"count_of_products: {count_of_products} in process_multiple_pages_data")
+        except psycopg2.Error as e:
+            logging.error(f"Error inserting data in process_multiple_pages_data: {e}")
+        logging.info("Data insertion completed in process_multiple_pages_data.")
 
 
-connect = create_connection()
-get_products_count(connect)
+    def process_single_page_data(self, url):
+        base_category_url = url
+        category_url = base_category_url
+        response = requests.get(base_category_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        category_name = soup.find("div", class_='topic__heading').find("h1").get_text()
+        count_of_products = len(soup.find_all("div", class_='inner_wrap TYPE_1'))
+        try:
+            date_of_insertion = datetime.date.today()
+
+            insert_to_ranking_products(category_name, count_of_products, category_url, date_of_insertion)
+
+            logging.info(f"Inserted data for category_name {category_name}, url - {category_url} "
+                             f"count_of_products: {count_of_products} in process_single_page_data")
+        except psycopg2.Error as e:
+            logging.error(f"Error inserting data in process_single_page_data: {e}")
+        logging.info("Data insertion completed in process_single_page_data.")
+
+
+    def find_duplicates(self):
+        find_duplicates()
+
+    def remove_duplicates(self):
+        remove_duplicates()
+
+
+obj = RankProds()
+obj.get_products_count()
+obj.find_duplicates()
+obj.remove_duplicates()
+
